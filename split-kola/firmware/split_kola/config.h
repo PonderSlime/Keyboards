3f2c9aa3da3f8881ba9206de9b36be3397414c21
PonderSlime (PC) <micah.j.edwards1@gmail.com>
2025-02-24 11:02:09 -0700

    Added the firmware, added base plate file template + Reference model

---
+++ split-kola/firmware/split_kola/config.h
@@ -0,0 +1,32 @@
+/*
+Copyright 2025 Micah Edwards
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define EE_HANDS
+
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
+#define SPLIT_POINTING_ENABLE
+#define SPLIT_USB_DETECT
+
+#define ENCODER_A_PINS_RIGHT { GP21}
+#define ENCODER_B_PINS_RIGHT { GP20 }
+
+#define ENCODER_A_PINS_LEFT { GP21}
+#define ENCODER_B_PINS_LEFT { GP20 }
---
+++ split-kola/firmware/split_kola/readme.md
@@ -0,0 +1,9 @@
+# Split-Kola
+
+A 5x7 (30-keys) split keyboard that uses a TRRS cable for the interconnect.
+
+* Keyboard Maintainer: [PonderSlime](https://github.com/ponderslime)
+
+## Features
+* Hotswap Keys
+* RGB Backlit Keys 
