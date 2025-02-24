e26e593404512884e94fa9e5218d86285c38ee4a
PonderSlime (PC) <micah.j.edwards1@gmail.com>
2025-02-24 12:38:28 -0700

    Changed keymap.c to be a little more readable

--- split-kola/firmware/split_kola/keymaps/default/keymap.c
+++ split-kola/firmware/split_kola/keymaps/default/keymap.c
@@ -20,29 +20,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,                             KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BACKSPACE,               \
         KC_DELETE, KC_GRAVE, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, \
-        KC_ESCAPE, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                      KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_NONUS_BACKSLASH, \
-                KC_Z, KC_X, KC_C, KC_V, KC_B,                                 KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,            \
-                KC_LEFT_SHIFT, KC_LEFT_CTRL, KC_LEFT_GUI, KC_SPACE,           KC_SPACE, KC_RIGHT_CTRL, KC_ENTER, KC_RIGHT_SHIFT
+        LT(1, KC_ESCAPE), KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                      KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_NONUS_BACKSLASH, \
+                KC_Z, KC_X, KC_C, KC_V, KC_B,                                   KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,            \
+                KC_LEFT_SHIFT, KC_LEFT_CTRL, KC_LEFT_GUI, KC_SPACE,             KC_SPACE, KC_RIGHT_CTRL, KC_ENTER, KC_RIGHT_SHIFT
     ),
     [1] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS        , \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           \
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,                            KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DELETE        , \
+        KC_INSERT, KC_PRINT_SCREEN, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, \
+        KC_NO, _______, _______, _______, _______, _______, _______,              KC_CAPS_LOCK, _______, _______, _______, _______, _______, _______, \
+                _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______,           \
+                KC_LEFT_SHIFT, KC_LEFT_CTRL, KC_LEFT_ALT, _______,                      _______, KC_RIGHT_CTRL, KC_RIGHT_ALT, KC_RIGHT_SHIFT
     ),
     [2] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS        , \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           \
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______        , \
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, \
+                _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______,           \
+                    _______, _______, _______, _______,                                 _______, _______, _______, _______
     ),
     [3] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS        , \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           \
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______        , \
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, \
+                _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______,           \
+                    _______, _______, _______, _______,                                 _______, _______, _______, _______
     )
 };
